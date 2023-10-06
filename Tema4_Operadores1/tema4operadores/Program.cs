9473105b0b69eb02a73b39f8afe3708ec5232335
Inseckt <oskii_11@hotmail.com>
2023-10-06 12:49:59 +0200

    Tema4_operadores

---
+++ Tema4_Operadores1/tema4operadores/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tema4operadores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //operadores matematicos
+            //% resto de la division entera:
+
+            int a = 9;
+            int resto = a % 2;
+
+            //Operadores de asignacion +=
+
+            int x = 1;
+
+            x += 1; //x = x+1;
+
+            x += 9; //x aqui vale 11
+            x -= 2; //x aqui vale 9
+
+             int resultado = 9;
+
+        }
+    }
+}
